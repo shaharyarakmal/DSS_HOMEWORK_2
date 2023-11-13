fbf4fc943ba1cabaaab9338b57012cca6f7eea63
Shaharyar Akmal <shaharyarakmal@gmail.com>
2023-11-13 21:31:51 +0100

    code with removed bugs and better user readability

--- math_quiz.py
+++ math_quiz.py
@@ -9,13 +9,21 @@ def function_A(min, max):
 
 
 def function_B():
+    """
+     returns random operators among +,_,*
+    """
     return random.choice(['+', '-', '*'])
 
 
 def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
+    """
+    takes three paramters, first two as integer, and third as an operator.
+    generates and returns problem and answer of a mathematical operation as defined
+
+    """
+    p = f"{n1} {o} {n2}" # string to print the problem generated from using function A and B
+    if o == '+': a = n1+n2 #addition logic was corrected it was defined to do subtraction initially
+    elif o == '-': a = n1 - n2 # subtraction logic was defined as addition so corrected
     else: a = n1 * n2
     return p, a
 
@@ -26,21 +34,21 @@ def math_quiz():
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for i in range(int(t_q)): #means this loop would run 3 times i-e we will be asked 3 maths questions'
+        n1 = function_A(1, 10); n2 = function_A(1,  int(5.5)); o = function_B()#n1, n2 select random integers, and o selects a operators defind in function_C'
 
         PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
+        print(f"\nQuestion: {PROBLEM}") #problem is printed as a string which was returned by function_C
         useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        useranswer = int(useranswer) #since all the variables are integer so we are just type casting user's answers into integer for beter comparison
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            s+= 1 #was initially s += -(-1) which is also the same and does s= s+1
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {s}/{int(t_q)}") #type casted t_q bcz to we earn integer points from answers so total points should be integer
 
 if __name__ == "__main__":
     math_quiz()
